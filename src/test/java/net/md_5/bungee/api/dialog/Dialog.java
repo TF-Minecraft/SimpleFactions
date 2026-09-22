45ddf3699d46e02cf25651a176bfa41f31c2b135
Ryan <7389646+ryanbarlow97@users.noreply.github.com>
2026-09-23 00:31:08 +0100

    build: compile against Paper API 1.21.10 (#22)

--- src/test/java/net/md_5/bungee/api/dialog/Dialog.java
+++ src/test/java/net/md_5/bungee/api/dialog/Dialog.java
@@ -1,7 +1,7 @@
 package net.md_5.bungee.api.dialog;
 
 /**
- * Test classpath stub for Spigot 1.21+ Player API references.
+ * Test classpath stub for the BungeeCord dialog type referenced by Player.
  */
 public interface Dialog {
 }
